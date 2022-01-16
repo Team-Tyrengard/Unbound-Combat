3617dc802603d398014cef1cb836c05611f875b2
hayakin <hayachikin@gmail.com>
2022-01-17 04:21:36 +0800

    Updated module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,7 +8,15 @@ module com.tyrengard.unbound.combat {
     requires com.tyrengard.aureycore.guis;
 
     exports com.tyrengard.unbound.combat;
+    exports com.tyrengard.unbound.combat.log;
+    exports com.tyrengard.unbound.combat.enums;
     exports com.tyrengard.unbound.combat.stats;
     exports com.tyrengard.unbound.combat.events;
+    exports com.tyrengard.unbound.combat.damage;
+    exports com.tyrengard.unbound.combat.effects;
+    exports com.tyrengard.unbound.combat.equipment;
     exports com.tyrengard.unbound.combat.combatant;
+    exports com.tyrengard.unbound.combat.combatant.skills;
+    exports com.tyrengard.unbound.combat.combatant.skills.resolvables;
+    exports com.tyrengard.unbound.combat.resistance;
 }
